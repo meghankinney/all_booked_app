4e0bade1029064c7a8a70251f10cc3e5850a2236
meghankinney <meghankinney@users.noreply.github.com>
2016-06-09 04:10:20 +0000

    after crash

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -5,6 +5,7 @@ validates :book_id, :presence => true
   
   
 has_many :votes , :class_name => "Vote", :foreign_key => "nomination_id"
+has_many :fans, :through => :votes, :source => :user
 belongs_to :book , :class_name => "Book", :foreign_key => "book_id"
 belongs_to :month , :class_name => "Month", :foreign_key => "month_id"
   

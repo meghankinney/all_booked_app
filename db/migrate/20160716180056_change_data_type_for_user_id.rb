66117fac03df6dacadddfe1ef151be1931b90002
meghankinney <meghankinney@users.noreply.github.com>
2016-07-16 18:05:59 +0000

    changed column type

---
+++ db/migrate/20160716180056_change_data_type_for_user_id.rb
@@ -0,0 +1,14 @@
+class ChangeDataTypeForUserId < ActiveRecord::Migration
+ 
+    
+   def self.up
+    change_column :votes, :user_id, :integer
+  end
+
+  def self.down
+    change_column :votes, :user_id, :string
+  end
+    
+    
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609190831) do
+ActiveRecord::Schema.define(version: 20160716180056) do
 
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 20160609190831) do
 
   create_table "votes", force: :cascade do |t|
     t.integer  "nomination_id"
-    t.string   "user_id"
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
